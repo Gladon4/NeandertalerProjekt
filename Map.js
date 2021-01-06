5399eac860d2ff7089486e4e29db691eb977ffc3
Gladon4 <alex.mario.grille.amg@gmail.com>
2021-01-06 10:45:11 +0100

    .

--- Map.js
+++ Map.js
@@ -1,4 +1,5 @@
 let data1;
+let data2;
 
 const mappa = new Mappa('Leaflet');
 let placeMap;
@@ -10,6 +11,10 @@ let lon;
 let d;
 let points;
 
+let important;
+
+const button = document.createElement('button');
+
 const options = {
   lat: 50,
   lng: 20,
@@ -17,9 +22,9 @@ const options = {
   style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
 }
 
-
 function preload() {
-  data1 = loadTable('places_1.csv', 'csv', 'header');
+  data1 = loadTable('places_important.csv', 'csv', 'header');
+  data2 = loadTable('places_all.csv', 'csv', 'header');
 }
 
 function setup() {
@@ -33,29 +38,49 @@ function setup() {
 
   points = [];
   d = [];
+
+  button.innerText = 'Kartenansich wechseln'
+  document.body.appendChild(button);
+  button.addEventListener('click', () => {changeMap();})
 }
 
 function draw() {
   clear();
-  i = 0
-  for (let row of data1.rows) {
-    d[i] = 10;
-    lat = row.get('lat');
-    lon = row.get('lon');
-    info = row.get('info')
-    const place = placeMap.latLngToPixel(lat, lon);
+  if (important) {
+    i = 0
+    for (let row of data1.rows) {
+      d[i] = 10;
+      lat = row.get('lat');
+      lon = row.get('lon');
+      info = row.get('info');
+      const place = placeMap.latLngToPixel(lat, lon);
 
-    if (mouseX < place.x + 5 && mouseX > place.x - 5 && mouseY < place.y + 5 && mouseY > place.y - 5) {
-      d[i] = 20;
-      fill(0, 0, 0);
-      textSize(20);
-      text(info , place.x, place.y + 20);
+      if (mouseX < place.x + 5 && mouseX > place.x - 5 && mouseY < place.y + 5 && mouseY > place.y - 5) {
+        d[i] = 20;
+        fill(0, 0, 0);
+        textSize(20);
+        text(info , place.x, place.y + 20);
+      }
+
+      fill(255, 50, 50);
+      points[i] = ellipse(place.x, place.y, d[i], d[i]);
+      i++;
     }
+  } else {
+      i = 0
+      for (let row of data2.rows) {
+        d[i] = 10;
+        lat = row.get('lat');
+        lon = row.get('lon');
+        const place = placeMap.latLngToPixel(lat, lon);
 
-
-    fill(255, 50, 50);
-    points[i] = ellipse(place.x, place.y, d[i], d[i]);
-    i++;
+        fill(255, 50, 50);
+        points[i] = ellipse(place.x, place.y, d[i], d[i]);
+        i++;
+    }
   }
+}
 
+function changeMap() {
+  important = !important;
 }
